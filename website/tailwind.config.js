ae85daa42bd0eb618ed339d543ed28b044dbda88
Md Rubel Rana <rubel.rana@northsouth.edu>
2024-06-03 02:18:58 +0600

    website added

---
+++ website/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.html', './index.html',],
+  theme: {
+    extend: {
+      fontFamily: {
+        'lobster': ['Lobster', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
