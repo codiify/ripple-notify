35148e9df8ddd1b14ba162ee2f1f8068ab8be11d
Md Rubel Rana <rubel.rana@northsouth.edu>
2024-06-07 10:57:34 +0600

    mouse interaction added

--- ripple-notify/ripple.js
+++ ripple-notify/ripple.js
@@ -1,37 +1,41 @@
-
 export const RippleConfig = {
-    timeout: 3000,
     animationSpeed: '0.5s',
-    duration: 500,
     colorful: true,
+    duration: 500,
     icon: true,
     position: 'bottom-right',
     progressBar: true,
+    timeout: 3000,
     colors: {
         success: {
             backgroundColor: '#ECFDF3',
             color: '#008A2E',
-            borderColor: '#D3FDE5'
+            borderColor: '#D3FDE5',
+            progressBarColor: '#008A2E',
         },
         error: {
             backgroundColor: '#FFF0F0',
             color: '#E60000',
-            borderColor: '#FFE0E1'
+            borderColor: '#FFE0E1',
+            progressBarColor: '#E60000',
         },
         info: {
             backgroundColor: '#F0F8FF',
             color: '#0973DC',
-            borderColor: '#D3E0FD'
+            borderColor: '#D3E0FD',
+            progressBarColor: '#0973DC',
         },
         warning: {
             backgroundColor: '#FFFCF0',
             color: '#DC7609',
-            borderColor: '#FDF5D3'
+            borderColor: '#FDF5D3',
+            progressBarColor: '#DC7609',
         },
         default: {
             backgroundColor: '#FFFFFF',
             color: '#171717',
-            borderColor: '#EDEDED'
+            borderColor: '#EDEDED',
+            progressBarColor: '#171717',
         }
     }
 };
