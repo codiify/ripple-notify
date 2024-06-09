2f850b54d52005ab40d0b35e458d7f5e3ec5deaf
Md Rubel Rana <rubel.rana@northsouth.edu>
2024-06-10 00:33:24 +0600

    documentation updated

--- test-laravel/resources/js/app.js
+++ test-laravel/resources/js/app.js
@@ -1,9 +1,6 @@
 import './bootstrap';
 
 import RippleNotify  from 'ripple-notify';
-import { RippleConfig } from '../../../ripple.js'; // Adjust the relative path as needed
-
-
 
 const ripple = RippleNotify;
 
